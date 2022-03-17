9e8229be157833b64ae6373854bd84f9e2cb05dd
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2022-03-16 18:21:52 -0700

    Add files via upload

---
+++ EEMBC/EEMBC_manual.py
@@ -0,0 +1,9 @@
+
+import sys
+
+from evtar.services.communicator.ux import Communicator
+
+
+##Start Execution script##
+Communicator.ExecuteCommandOnTarget(r'C:\users\Local_Admin\Documents\Autofiles\Dut_Side\EEMBC.bat')
+
---
+++ Icob/ICOB_manual.py
@@ -0,0 +1,11 @@
+
+import sys
+import time
+
+from evtar.services.communicator.ux import Communicator
+
+##Start Execution script##
+Communicator.ExecuteCommandOnTarget(r'C:\tools\elevate\elevate.exe -c C:\users\Local_Admin\Documents\TransferToSUT\Autofiles\Dut_Side\Icob.vbs')
+
+
+
