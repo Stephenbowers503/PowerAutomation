f50b968e9d305f10e57f658005e225cfa42dbedb
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:55:42 -0800

    Delete 2160p directory

--- 2160p/2160p_manual.py
+++
@@ -1,6 +0,0 @@
-import sys
-from evtar.services.communicator.ux import Communicator
-
-##Start Execution script##
-Communicator.ExecuteCommandOnTarget(r'C:\tools\elevate\elevate.exe -c C:\users\Local_Admin\Documents\TransferToSUT\Autofiles\Dut_Side\2160p.ps1')
-
--- 2160p/Run_2160p.bat
+++
@@ -1,3 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\2160p\\2160p_manual.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\2160p\\2160p_Pacs.py
-start powershell "C:\python36\lib\site-packages\evtar\maple\workloads\2160p\2160p_Progress_Bar.ps1"
