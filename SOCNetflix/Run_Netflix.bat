71d7c9fb822ea80ebc148b7bb38b1490431e5ad4
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 13:15:23 -0800

    Delete SOCNetflix directory

--- SOCNetflix/Run_Netflix.bat
+++
@@ -1,2 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\SOCNetflix\\Netflix_manual.py
-start powershell "C:\python36\lib\site-packages\evtar\maple\workloads\SOCNetflix\SOCNetflix_Progress_Bar.ps1"
