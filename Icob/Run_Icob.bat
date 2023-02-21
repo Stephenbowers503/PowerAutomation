69c27b74aa061c594f3b88bfdc4b2589bc59e1b1
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:58:13 -0800

    Delete Icob directory

--- Icob/Run_Icob.bat
+++
@@ -1,3 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\ICOB\\ICOB_manual.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\ICOB\\ICOB_Pacs.py
-start powershell "C:\python36\lib\site-packages\evtar\maple\workloads\Icob\ICOB_Progress_Bar.ps1"
