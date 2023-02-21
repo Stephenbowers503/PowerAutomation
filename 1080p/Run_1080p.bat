53ca824950ad9fd9226dfaab7ba31207d05d2d24
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:55:03 -0800

    Delete 1080p directory
    deleting old code

--- 1080p/Run_1080p.bat
+++
@@ -1,3 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\1080p\\1080p_manual.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\1080p\\1080p_Pacs.py
-Start powershell "C:\python36\lib\site-packages\evtar\maple\workloads\1080p\1080p_Progress_Bar.ps1"
