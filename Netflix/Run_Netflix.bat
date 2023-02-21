9a430a22ce9db811e42edda28f51f13edd615a32
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:59:56 -0800

    Delete Netflix directory

--- Netflix/Run_Netflix.bat
+++
@@ -1,3 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\Netflix\\Netflix_manual.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\Netflix\\Netflix_Pacs.py
-start Powershell "C:\python36\lib\site-packages\evtar\maple\workloads\Netflix\NETFLIX_Progress_Bar.ps1"
