2294dfd9dd6bb31930104f7bb71d8c252eba7187
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 13:13:59 -0800

    Delete SOC1080p directory

--- SOC1080p/Run_1080p.bat
+++
@@ -1,2 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\SOC1080p\\1080p_manual.py
-start powershell "C:\python36\lib\site-packages\evtar\maple\workloads\SOC1080p\SOC1080p_Progress_Bar.ps1"
