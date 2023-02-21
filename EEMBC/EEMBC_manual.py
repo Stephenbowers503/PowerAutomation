823d44835d20bfed7fc571cc4d793aa451ad1fb0
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:57:20 -0800

    Delete EEMBC directory

--- EEMBC/EEMBC_manual.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-
-from evtar.services.communicator.ux import Communicator
-
-
-##Start Execution script##
-Communicator.ExecuteCommandOnTarget(r'C:\users\Local_Admin\Documents\Autofiles\Dut_Side\EEMBC.bat')
-
--- EEMBC/Run_EEMBC.bat
+++
@@ -1,4 +0,0 @@
-python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\EEMBC\\Reset_Coms.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\EEMBC\\EEMBC_manual.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\EEMBC\\EEMBC_Pacs.py
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\EEMBC\\unplug_750s.py
