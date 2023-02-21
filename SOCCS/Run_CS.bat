7c5311ae81adf8466113862daab8fdbc7b61134d
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 13:14:41 -0800

    Delete SOCCS directory

--- SOCCS/Run_CS.bat
+++
@@ -1,4 +0,0 @@
-start python C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\SOCCS\\CS_manual.py
-Start Powershell "C:\\python36\\lib\\site-packages\\evtar\\maple\\workloads\\SOCCS\\CS_Progress_Bar.ps1"
-
-
