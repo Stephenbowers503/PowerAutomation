f373202e4fb979924c57467632f37da560fc069d
Stephenbowers503 <101761200+Stephenbowers503@users.noreply.github.com>
2023-02-21 12:59:01 -0800

    Delete MM25 directory

--- MM25/MM25_manual.py
+++
@@ -1,7 +0,0 @@
-import sys
-from evtar.services.communicator.ux import Communicator
-
-Communicator.ExecuteCommandOnTarget(r' C:\tools\elevate\elevate.exe -c C:\users\Local_Admin\Documents\TransferToSUT\Autofiles\Dut_Side\Run_MM25.ps1')
-
-
-exit()
